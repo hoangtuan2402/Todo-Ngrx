58f8c2beac0109fb80bd7095d58b0fb87306be39
Anhht_is <anhht@is.viettel.com.vn>
2022-04-28 09:11:48 +0700

    Styling Component

--- src/app/modules/todo/todo.module.ts
+++ src/app/modules/todo/todo.module.ts
@@ -2,6 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TodoMainComponent } from './pages/todo-main/todo-main.component';
 import { Routes, RouterModule } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
+import { TodoComponent } from './components/todo/todo.component';
+import { TodoFormComponent } from './components/todo-form/todo-form.component';
 
 const routes: Routes = [
   {
@@ -13,7 +17,11 @@ const routes: Routes = [
 
 @NgModule({
   declarations: [
-    TodoMainComponent
+    TodoMainComponent,
+    HeaderComponent,
+    TodoListComponent,
+    TodoComponent,
+    TodoFormComponent
   ],
   imports: [
     CommonModule, RouterModule.forChild(routes)
